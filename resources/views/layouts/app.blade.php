ca432533108f7273d731ae5050bd3475cea0b3da
wahyush04 <wahyush04@gmail.com>
2021-06-28 21:17:04 +0800

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,7 @@
                                     @endif
 
                                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                            document.getElementById('logout-form').submit();">
+                                                            document.getElementBy Id('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
                                     
