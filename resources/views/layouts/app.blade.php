51519114831b870e1b1b8673d35ede51f4ea964f
David Geri <davidgeri988@gmail.com>
2021-06-28 20:32:24 +0800

    warna

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color: #FB7AFC">
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color: #66DE93">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
                     {{ config('app.name', 'Laravel') }}
