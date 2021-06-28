d522013fec31c81e84426cab3d94854b1bbc930b
David Geri <davidgeri988@gmail.com>
2021-06-28 21:25:43 +0800

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,44 +102,12 @@
 
     <!-- Footer -->
     <br><br><br>
-<footer class="text-center text-lg-start bg-light text-muted">
+<footer class="text-center text-lg-start text-muted" style="background-color: #444444">
   <!-- Section: Social media -->
-  <section
-    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
-  >
-    <!-- Left -->
-    <div class="me-5 d-none d-lg-block">
-      <span>Get connected with us on social networks:</span>
-    </div>
-    <!-- Left -->
-
-    <!-- Right -->
-    <div>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-google"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-instagram"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-linkedin"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-github"></i>
-      </a>
-    </div>
-    <!-- Right -->
-  </section>
   <!-- Section: Social media -->
 
   <!-- Section: Links  -->
-  <section class="">
+  <section class="text-white pt-3">
     <div class="container text-center text-md-start mt-5">
       <!-- Grid row -->
       <div class="row mt-3">
@@ -147,36 +115,15 @@
         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
           <!-- Content -->
           <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>KuliTech
+            <i class="fas fa-gem me-3 mr-2"></i>KuliTech
           </h6>
           <p>
           Kuli Tech adalah perkumpulan dari orang-orang kreatif yang siap untuk menciptakan dan
-            mengimplementasikan banyak hal baru yang inovatif. Kami bersama-sama bukan hanya sekedar belajar, tapi kami juga
-            bersama melatih skill di berbagai bidang, seperti programming dan gaming. Menyenangkan bukan?
+            mengimplementasikan banyak hal baru yang inovatif.
           </p>
         </div>
         <!-- Grid column -->
 
-        <!-- Grid column -->
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            Products
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Angular</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">React</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Vue</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Discord</a>
-          </p>
-        </div>
-        <!-- Grid column -->
 
         <!-- Grid column -->
         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
@@ -191,10 +138,10 @@
             <a href="#!" class="text-reset">Customer Service</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Orders</a>
+            <a href="#!" class="text-reset">Blog</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Help</a>
+            <a href="#!" class="text-reset">Discord</a>
           </p>
         </div>
         <!-- Grid column -->
@@ -208,7 +155,7 @@
           <p><i class="fas fa-home me-3"></i> Mataram, NTB, Indonesia</p>
           <p>
             <i class="fas fa-envelope me-3"></i>
-            KuliStore@Gmail.com
+            KuliStore@gmail.com
           </p>
           <p><i class="fas fa-phone me-3"></i> +6281 234 567 88</p>
           <p><i class="fas fa-print me-3"></i> + 6281 234 567 89</p>
@@ -221,7 +168,7 @@
   <!-- Section: Links  -->
 
   <!-- Copyright -->
-  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+  <div class="text-center p-4 text-white" style="background-color: rgba(0, 0, 0, 0.05);">
     © 2021 Copyright:
     <a class="text-reset fw-bold" href="https://mdbootstrap.com/">KuliStore</a>
   </div>
