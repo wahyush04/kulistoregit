b2b7a6f254c4a0de9c3a19d42f6f8dbbf27298a4
David Geri <davidgeri988@gmail.com>
2021-06-29 22:08:54 +0800

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -36,10 +36,22 @@
 			<ul class="nav navbar-nav navbar-left">
 				<li><a href="{{ route('shop.index') }}" class="nav-link">Store</a></li>
 				<li><a class="nav-link" href="{{ url('/about')}}">About</a></li>
-                <li>@livewire('shop.cartnav')</li>
+			</ul>
+
+			<ul class="nav navbar-right">
+                <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                </a></li>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+                </form>
 			</ul>
 		</div>
 	</div>
+
+
 </section>
 
 <!-- about section -->
