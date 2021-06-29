137b453debae5a0921a7668caf8b06a85ff16a83
David Geri <davidgeri988@gmail.com>
2021-06-29 21:27:26 +0800

    product

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@
                         <li>                            
                             <a class="nav-link" href="{{ url('/about')}}">About</a>
                         </li>
-                        @livewire('shop.cartnav')
+                       
                     </ul>
 
 
@@ -70,12 +70,13 @@
                                 </li>
                             @endif
                         @else
+                             @livewire('shop.cartnav')
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-
+                            
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="/profile">Profile</a>
 
